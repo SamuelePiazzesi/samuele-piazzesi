62516d552078a5e34ac68684dc0e40b95ce75e74
Samuele Piazzesi <samuelepiazzesi@gmail.com>
2022-12-14 11:44:45 +0100

    fix: typo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -36,7 +36,7 @@ export default function App({ Component, pageProps }: AppProps) {
 					name="description"
 					content="Full Stack Engineer who loves build things. I'm a tech passionate, guitar evangelist and food enthusiast. Check out my projects, or write me on Socials"
 				/>
-				<meta name="image" content="https://samuelepiazzesi.com/portait.jpg" />
+				<meta name="image" content="https://samuelepiazzesi.com/portrait.jpg" />
 
 				{/* <Facebook Meta Tags --> */}
 				<meta property="og:url" content="samuelepiazzesi.com" />
@@ -48,7 +48,7 @@ export default function App({ Component, pageProps }: AppProps) {
 				/>
 				<meta
 					property="og:image"
-					content="https://samuelepiazzesi.com/portait.jpg"
+					content="https://samuelepiazzesi.com/portrait.jpg"
 				/>
 
 				<meta name="twitter:card" content="summary_large_image" />
@@ -59,7 +59,7 @@ export default function App({ Component, pageProps }: AppProps) {
 				/>
 				<meta
 					name="twitter:image"
-					content="https://samuelepiazzesi.com/portait.jpg"
+					content="https://samuelepiazzesi.com/portrait.jpg"
 				/>
 				<link
 					rel="icon"
