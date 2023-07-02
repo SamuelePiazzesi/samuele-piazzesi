dc42914f12b8532cc835d58a496df0f768648e98
Samuele Piazzesi <samuelepiazzesi@gmail.com>
2023-07-02 12:06:46 +0200

    add govtree

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,37 +1,37 @@
 export type Card = {
-	id: number;
-	title: string;
-	description: string;
-	link: string;
+  id: number;
+  title: string;
+  description: string;
+  link: string;
 };
 
 export const cards: Card[] = [
-	{
-		id: 1,
-		title: "Nextjs Hotjar",
-		description:
-			"Plugin that allows you to easily integrate Hotjar in your Next.js application.",
-		link: "https://github.com/SamuelePiazzesi/nextjs-hotjar",
-	},
-	{
-		id: 2,
-		title: "Solito Supabase Template",
-		description:
-			"Solito Library (React Native + Next.js, unified) template, wrapped with Supabase and authentication handler.",
-		link: "https://github.com/SamuelePiazzesi/Solito-Supabase-Template",
-	},
-	{
-		id: 3,
-		title: "Memes bot",
-		description:
-			"Telegram bot that sends memes, filtered by hotness and other cool features taken from /memes subreddit.",
-		link: "https://github.com/SamuelePiazzesi/MemesBot",
-	},
-	{
-		id: 4,
-		title: "Rinald",
-		description:
-			"Netflix-ish website made with some cool libraries. Do you know Rinald? He's a cool guy.",
-		link: "https://rinaldsefa.com",
-	},
+  {
+    id: 1,
+    title: "GovTree",
+    description:
+      "It's like linktree, but for Italian Public Services. It's a website that allows you to easily find the specific website for your taxes declaration (Yes i pay taxes).",
+    link: "https://govtree.info",
+  },
+  {
+    id: 1,
+    title: "Nextjs Hotjar",
+    description:
+      "Plugin that allows you to easily integrate Hotjar in your Next.js application.",
+    link: "https://github.com/SamuelePiazzesi/nextjs-hotjar",
+  },
+  {
+    id: 3,
+    title: "Memes bot",
+    description:
+      "Telegram bot that sends memes, filtered by hotness and other cool features taken from /memes subreddit.",
+    link: "https://github.com/SamuelePiazzesi/MemesBot",
+  },
+  {
+    id: 4,
+    title: "Rinald",
+    description:
+      "Netflix-ish website made with some cool libraries. Do you know Rinald? He's a cool guy.",
+    link: "https://rinaldsefa.com",
+  },
 ];
